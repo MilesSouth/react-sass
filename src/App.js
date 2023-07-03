6e507964f0dd5d7d902acaaafd6faaf8ab4cc96e
SelimGüney <slmgny333@gmail.com>
2023-07-03 22:35:40 +0300

    header footer scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Header from "./components/Header"
+import Card from "./components/Card"
+import Footer from "./components/Footer"
 import "./sass/App.scss"
 
 function App() {
   return (
-    
-  );
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
