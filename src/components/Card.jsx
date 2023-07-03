b37ebb6f24464753bd2f9023f18faf55759a7db2
SelimGüney <slmgny333@gmail.com>
2023-07-03 23:09:17 +0300

    classes edited

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,14 @@ import CardStyle from "../sass/card.module.scss"
 const Card = ({ item }) => {
   return (
     <div className={CardStyle["card"]}>
-      <div className="name">
-        <span className="name-content">{item.name}</span>
+      <div className={CardStyle["name"]}>
+        <span className={CardStyle["name-content"]}>{item.name}</span>
       </div>
-      <div className="job">
-        <span className="job-content">{item.job}</span>
+      <div className={CardStyle["job"]}>
+        <span className={CardStyle["job-content"]}>{item.job}</span>
       </div>
-      <div className="comment">
-        <span className="comment-content">{item.comment}</span>
+      <div className={CardStyle["comment"]}>
+        <span className={CardStyle["comment-content"]}>{item.comment}</span>
       </div>
       <div className={CardStyle.image}>
         <img src={item.img} alt="" width={"300rem"} />
