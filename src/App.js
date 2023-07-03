3ce3cdde0db0ef5a54f14097bc6a32dba2e671b9
SelimGüney <slmgny333@gmail.com>
2023-07-03 23:08:08 +0300

    buttons added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Header from "./components/Header"
 import Card from "./components/Card"
 import Footer from "./components/Footer"
 import "./sass/App.scss"
+import data from "./data"
 
 function App() {
   return (
     <>
       <Header />
-      <Card />
+      {data.map((item) => {
+        return <Card key={item.id} item={item} />
+      })}
       <Footer />
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,24 @@
-const Card = () => {
+import CardStyle from "../sass/card.module.scss"
+
+const Card = ({ item }) => {
   return (
-    <div>
-      <h2>Card</h2>
+    <div className={CardStyle["card"]}>
+      <div className="name">
+        <span className="name-content">{item.name}</span>
+      </div>
+      <div className="job">
+        <span className="job-content">{item.job}</span>
+      </div>
+      <div className="comment">
+        <span className="comment-content">{item.comment}</span>
+      </div>
+      <div className={CardStyle.image}>
+        <img src={item.img} alt="" width={"300rem"} />
+      </div>
+      <div className={CardStyle.buttons}>
+        <button className={CardStyle.small}>Small</button>
+        <button className={CardStyle.large}>Large</button>
+      </div>
     </div>
   )
 }
